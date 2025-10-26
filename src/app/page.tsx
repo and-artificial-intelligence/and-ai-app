43be0359f98f20d560a57f141355c854aabaf37b
Caleb <46455007+Yuppyboy@users.noreply.github.com>
2025-10-26 17:05:43 -0500

    Fix image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default function Home() {
             fill
             alt="landing-page-bg"
             className="scale-110 object-contain"
-            src="/illustration-2.png"
+            src="/Illustration-2.png"
           />
         </div>
       </section>
