936321ebf9c864980852c540983048beee37de13
Micah Skinner <skinnermicah@gmail.com>
2026-01-29 16:01:13 -0800

    Update index.ts

--- src/common/components/structured-data/index.ts
+++ src/common/components/structured-data/index.ts
@@ -1,14 +1,14 @@
-export { JsonLd } from './json-ld';
+export { generateArticleSchema, type ArticleSchemaProps } from './article';
+export { generateBreadcrumbSchema, type BreadcrumbItem } from './breadcrumb';
 export { generateFAQPageSchema, type FAQItem } from './faq-page';
+export { JsonLd } from './json-ld';
 export { generateOrganizationSchema } from './organization';
+export {
+  generatePersonListSchema,
+  generatePersonSchema,
+  type PersonSchemaProps,
+} from './person';
 export {
   generateSoftwareApplicationSchema,
   type PricingOffer,
 } from './software-application';
-export { generateArticleSchema, type ArticleSchemaProps } from './article';
-export { generateBreadcrumbSchema, type BreadcrumbItem } from './breadcrumb';
-export {
-  generatePersonSchema,
-  generatePersonListSchema,
-  type PersonSchemaProps,
-} from './person';
