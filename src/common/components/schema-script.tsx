7ea914853b3fccb72b0dc5a498ab6dfa59349c4f
Amanda Li <dabao01810@gmail.com>
2026-02-22 19:31:06 -0800

    Remediate XSS alerts and harden npm installs (#39)
    * Remediate XSS findings and drop npm lockfile
    
    * Restore npm lockfile and enforce ignore-scripts

--- src/common/components/schema-script.tsx
+++ src/common/components/schema-script.tsx
@@ -2,6 +2,15 @@ interface SchemaScriptProps {
   schema: object | object[];
 }
 
+function serializeSchema(schema: object): string {
+  return JSON.stringify(schema)
+    .replace(/</g, '\\u003c')
+    .replace(/>/g, '\\u003e')
+    .replace(/&/g, '\\u0026')
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029');
+}
+
 /**
  * Component to inject JSON-LD structured data into the page.
  * Uses a regular script tag to ensure schema is present in initial HTML for crawlers.
@@ -14,12 +23,11 @@ export function SchemaScript({ schema }: SchemaScriptProps) {
       {schemas.map((s, index) => (
         <script
           key={index}
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify(s),
-          }}
           id={`schema-${index}`}
           type="application/ld+json"
-        />
+        >
+          {serializeSchema(s)}
+        </script>
       ))}
     </>
   );
