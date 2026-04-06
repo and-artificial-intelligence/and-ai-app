65e5023dcba815dd42afa28bd7d4fcef24f224da
Micah Skinner <skinnermicah@gmail.com>
2026-04-06 16:02:13 -0500

    advisory refactor

--- src/module/advisory-board/index.ts
+++ src/module/advisory-board/index.ts
@@ -1,2 +1,3 @@
-export type { Advisor } from './components';
+export { advisorSchemaData } from './advisors';
+export type { Advisor } from './advisors';
 export { AdvisoryBoardSection } from './components';
